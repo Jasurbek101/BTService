25b1eaefbf1c9f29e745eb16080caa80072ab591
Jahongir <jaxongir10112000@gmail.com>
2023-05-08 20:32:05 +0500

    category entity add price property

--- src/main/java/uz/BTService/btservice/entity/Category.java
+++ src/main/java/uz/BTService/btservice/entity/Category.java
@@ -1,10 +1,14 @@
 package uz.BTService.btservice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 import uz.BTService.btservice.constants.TableNames;
 import uz.BTService.btservice.entity.base.BaseServerModifierEntity;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,4 +19,5 @@ import uz.BTService.btservice.entity.base.BaseServerModifierEntity;
 @Table(name = TableNames.CATEGORY)
 public class Category extends BaseServerModifierEntity {
     private String name;
+    private Double price;
 }
