ed28662d63d9a1493dac14982a94f9e7062f7087
Jahongir <jaxongir10112000@gmail.com>
2023-06-08 11:12:38 +0500

    Send sms phone number

---
+++ src/main/java/uz/BTService/btservice/smsForPhone/SmsPojo.java
@@ -0,0 +1,20 @@
+package uz.BTService.btservice.smsForPhone;
+
+public class SmsPojo {
+
+    private String to;
+    private String message;
+
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
